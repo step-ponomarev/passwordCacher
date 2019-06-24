0a8b3ae4d423a5f06f6517e53e1bdbc99cbc3e84
Ponomarev Stepan <htconesuper11@gmail.com>
2019-06-24 22:08:20 +0300

    console prototype

--- passwordCacher.java
+++ passwordCacher.java
@@ -2,19 +2,29 @@ import java.util.ArrayList;
 import java.io.*;
 import java.util.Scanner;
 
-class Cacher {
+interface Controler {
+  //void menu();
+  //void generateID();
+  void add();
+  void remove(int index);
+  void clean();
+  void printAll();
+  void exit();
+};
+
+class Cacher implements Controler {
   private ArrayList<String> m_site;
   private ArrayList<String> m_pass;
   private int m_numb;
   private String m_path = "./passwords.txt";
   private void checkData() {
-    //File file = new File(m_path);
+
   }
   public Cacher() {
     try {
       File cfile = new File(m_path);
 
-      if (!cfile.exists()) {
+      if (!cfile.exists() || (cfile.length() == 0)) {
         m_numb = 0;
         cfile.createNewFile();
 
@@ -38,15 +48,71 @@ class Cacher {
       System.out.println("File Error");
     }
   }
-  
-  public void print() {
-    for (int k = 0; k < m_numb; ++k) {
-      System.out.println(m_site.get(k) + " : " + m_pass.get(k));
+
+  public void add() {
+    Scanner in = new Scanner(System.in);
+    String dataSite = new String();
+    String dataPass = new String();
+
+    System.out.println("______________________");
+    System.out.println("Для отмены введите 'q'");
+    System.out.println("----------------------");
+
+    System.out.println("Введите название сервиса:");
+    dataSite = in.nextLine();
+    if (dataSite.equals("q")) {
+      return;
     }
+
+    System.out.println("Введите пароль:");
+    dataPass = in.nextLine();
+    if (dataPass.equals("q")) {
+      return;
+    }
+
+    m_site.add(m_numb, dataSite);
+    m_pass.add(m_numb, dataPass);
+    m_numb++;
   }
 
-  public void addPass() {
+  public void remove(int index) {
+    if (index < 0 || index >= m_numb) {
+      System.out.println("Неверный индекс");
+      return;
+    } else if (m_numb == 0) {
+      System.out.println("Список пуст");
+      return;
+    }
 
+    ArrayList<String> c_site = new ArrayList();
+    ArrayList<String> c_pass = new ArrayList();
+    for (int k = 0; k < m_numb; ++k) {
+      if (k != index) {
+        c_site.add(m_site.get(k));
+        c_pass.add(m_pass.get(k));
+      }
+    }
+
+    m_site = c_site;
+    m_pass = m_pass;
+    m_numb--;
+  }
+
+  public void clean() {
+    m_numb = 0;
+    m_site.clear();
+    m_pass.clear();
+  }
+
+  public void printAll() {
+    if (m_numb == 0) {
+      System.out.println("Список пуст");
+      return;
+    }
+    System.out.println("сервис : пароль (номер)");
+    for (int k = 0; k < m_numb; ++k) {
+      System.out.println(m_site.get(k) + " : " + m_pass.get(k) + " (" + (k + 1) + ")");
+    }
   }
 
   public void exit() {
@@ -67,13 +133,15 @@ class Cacher {
 
 class Launcher {
   public static void main(String [] args) {
-    Cacher cache = new Cacher();
-    /*String user_input = "";
-    while (user_input != "q") {
+    Controler controler = new Cacher();
 
-    }*/
-    cache.print();
 
-    cache.exit();
+    controler.printAll();
+    controler.add();
+    controler.printAll();
+    //controler.remove();
+    controler.printAll();
+
+    controler.exit();
   }
 }
